c841086a592a0ddccb8b73a083861d56cac2a587
AceShiro <aceshiro.117@gmail.com>
2018-05-13 19:18:15 +0000

    v0.3.3

--- resources/views/partials/_team.blade.php
+++ resources/views/partials/_team.blade.php
@@ -10,14 +10,14 @@
     <div class="row">
       <div class="col-sm-6">
         <div class="team-member">
-          <img class="mx-auto rounded-circle" src="img/team/pauline.jpg" alt="">
+          <img class="mx-auto rounded-circle" src="img/team/sylvain_team.jpg" alt="">
           <h4>Sylvain</h4>
           <p class="text-muted">Directeur Général</p>
         </div>
       </div>
       <div class="col-sm-6">
         <div class="team-member">
-          <img class="mx-auto rounded-circle" src="img/team/pauline.jpg" alt="">
+          <img class="mx-auto rounded-circle" src="img/team/pauline_team.jpg" alt="">
           <h4>Pauline</h4>
           <p class="text-muted">Présidente</p>
         </div>
